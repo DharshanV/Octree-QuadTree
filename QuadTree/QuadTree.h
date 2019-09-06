4a0b0ef7c63f55e6c7dbfdaf689de3b45e0e8056
DharshanV <38626675+DharshanV@users.noreply.github.com>
2019-09-05 21:30:40 -0700

    Implemented Quadtree with search area feature

--- QuadTree/QuadTree.h
+++ QuadTree/QuadTree.h
@@ -1,57 +1,159 @@
 #pragma once
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <vector>
+#include <list>
 typedef unsigned int uint;
+using namespace std;
 
 struct Point {
 public:
 	Point() : x(0), y(0) {}
 	Point(float x, float y) : x(x), y(y) {}
-private:
 	float x;
 	float y;
 };
 
 struct Boundary {
 public:
+	Boundary() :topLeft(0,0),bottomRight(0,0) { }
 	Boundary(const Point& topLeft, const Point& bottomRight) : 
 		topLeft(topLeft),bottomRight(bottomRight) {
 
 	}
-private:
+	bool contains(const Point& point) const {
+		if ((point.x >= topLeft.x && point.x <= bottomRight.x) &&
+			(point.y >= topLeft.y && point.y <= bottomRight.y)) {
+			return true;
+		}
+		return false;
+	}
+
+	bool intersects(const Boundary& boundary) {
+		return (topLeft.x < boundary.bottomRight.x) && 
+			(bottomRight.x > boundary.topLeft.x) &&
+			(topLeft.y < boundary.bottomRight.y) && 
+			(bottomRight.y > boundary.topLeft.y);
+	}
+
+	float getWidth() {
+		return fabs(topLeft.x - bottomRight.x);
+	}
+
+	float getHeight() {
+		return fabs(topLeft.y - bottomRight.y);
+	}
+
 	Point topLeft;
 	Point bottomRight;
 };
 
 class QuadTree {
 public:
-	QuadTree(const Boundary& bounds, uint maxCount) : 
-		bounds(bounds),maxCount(maxCount) {
-
+	QuadTree(const Boundary& bound, uint maxCount) :
+		bound(bound),maxCount(maxCount) {
+		points.reserve(maxCount);
+		divided = false;
+		NW = NULL;
+		NE = NULL;
+		SW = NULL;
+		SE = NULL;
 	}
 
 	void insert(const Point& point) {
-		
+		//if it doesn't contain then the point is out of bounds
+		if (!bound.contains(point)) return;
+		//if there is size then add it
+		if (points.size() < maxCount) { 
+			points.push_back(point); 
+			return;
+		}
+		//at this point we need to sub divide
+		if(!divided) subdivide(point);
+		NW->insert(point);
+		NE->insert(point);
+		SW->insert(point);
+		SE->insert(point);
 	}
 
-	void draw(sf::RenderWindow* window) {
-
+	void searchArea(const Boundary& boundary,list<Point>& searchPoints) {
+		if (!bound.intersects(boundary)) return;
+		for (Point& p : points) {
+			if (boundary.contains(p)) searchPoints.push_back(p);
+		}
+		if (!divided) return;
+		NW->searchArea(boundary, searchPoints);
+		NE->searchArea(boundary, searchPoints);
+		SW->searchArea(boundary, searchPoints);
+		SE->searchArea(boundary, searchPoints);
 	}
 
-	~QuadTree() { 
+	void draw(sf::RenderWindow& window) {
+		window.draw(getBox());
+		for (Point& p : points) {
+			window.draw(getPoint(p));
+		}
+		if (!divided) return;
+		NW->draw(window);
+		NE->draw(window);
+		SW->draw(window);
+		SE->draw(window);
+	}
+
+	sf::RectangleShape getBox() {
+		sf::RectangleShape box;
+		float w, h;
+		w = fabs(bound.topLeft.x - bound.bottomRight.x);
+		h = fabs(bound.topLeft.y - bound.bottomRight.y);
+		box.setSize(sf::Vector2f(w, h));
+		box.setPosition(sf::Vector2f(bound.topLeft.x, bound.topLeft.y));
+		box.setOutlineColor(sf::Color(sf::Color::White));
+		box.setFillColor(sf::Color::Transparent);
+		box.setOutlineThickness(boxOutlineSize);
+		return box;
+	}
+
+	sf::CircleShape getPoint(const Point& point) {
+		sf::CircleShape circle(dotRadius);
+		circle.setOrigin(-sf::Vector2f(point.x- dotRadius, point.y- dotRadius));
+		circle.setFillColor(sf::Color::Red);
+		return circle;
+	}
+
+	void subdivide(const Point& point) {
+		Boundary NWB, NEB, SWB, SEB;
+
+		float dw = fabs(bound.topLeft.x - bound.bottomRight.x) / 2;
+		float dh = fabs(bound.topLeft.y - bound.bottomRight.y) / 2;
+
+		NWB = Boundary(bound.topLeft, Point(bound.topLeft.x + dw, bound.topLeft.y + dh));
+		NEB = Boundary(Point(bound.topLeft.x+dw, bound.topLeft.y), Point(bound.topLeft.x + dw*2, bound.topLeft.y + dh));
+		SWB = Boundary(Point(bound.topLeft.x , bound.topLeft.y + dh), Point(bound.topLeft.x + dw, bound.topLeft.y + dh * 2));
+		SEB = Boundary(Point(bound.topLeft.x + dw, bound.topLeft.y + dh), bound.bottomRight);
+
+		NW = new QuadTree(NWB, maxCount);
+		NE = new QuadTree(NEB, maxCount);
+		SW = new QuadTree(SWB, maxCount);
+		SE = new QuadTree(SEB, maxCount);
+		divided = true;
+	}
+
+	~QuadTree() {
+		if (!divided) return;
 		delete NW;
 		delete NE;
 		delete SW;
 		delete SE;
 	}
 private:
-	vector<Point> myData;
-	Boundary bounds;
+	vector<Point> points;
+	Boundary bound;
 	uint maxCount;
 	QuadTree* NW;
 	QuadTree* NE;
 	QuadTree* SW;
 	QuadTree* SE;
+	bool divided;
+	float boxOutlineSize = 1;
+	float dotRadius = 3;
 };
 
--- QuadTree/main.cpp
+++ QuadTree/main.cpp
@@ -4,40 +4,77 @@
 using namespace std;
 using namespace sf;
 void processEvents(RenderWindow& window);
+float random(float min, float max);
+sf::CircleShape getPoint(const Point& point);
+
+uint width = 500;
+uint height = 500;
+QuadTree tree(Boundary(Point(5, 5), Point(width - 5, height - 5)), 4);
+Boundary searchArea;
+RectangleShape searchBox;
 
 int main() {
-	uint width = 700;
-	uint height = 700;
-	sf::RenderWindow window(sf::VideoMode(700, 500), "QuadTree");
-	QuadTree tree(Boundary(Point(0, 0), Point(width, height)),4);
+	srand(time(0));
+	sf::RenderWindow window(sf::VideoMode(width, height), "QuadTree");
 
+	for (int i = 0; i < 1000; i++) {
+		tree.insert(Point(random(5, width - 5), random(5, height - 5)));
+	}
+
+	searchBox.setFillColor(Color::Transparent);
+	searchBox.setOutlineThickness(2);
+	searchBox.setOutlineColor(Color::Green);
+	searchBox.setSize(Vector2f(50,50));
+	searchBox.setPosition(50, 50);
+
+	list<Point> query;
 	while (window.isOpen()) {
+		tree.searchArea(searchArea,query);
+
 		processEvents(window);
-
-
 		window.clear();
-		tree.draw(&window);
+		tree.draw(window);
+		for (Point& p : query) {
+			window.draw(getPoint(p));
+		}
+		query.clear();
+		window.draw(searchBox);
 		window.display();
 	}
 	return 0;
 }
 
+float random(float min, float max) {
+	return (float)(rand() % (int)max + min);
+}
+
 void processEvents(RenderWindow& window) {
 	sf::Event event;
-	while (window.pollEvent(event)) {
-		switch (event.type) {
-		case Event::Closed:
+	window.pollEvent(event);
+	if (event.type == Event::Closed) {
+		window.close();
+	}
+	if (event.type == Event::KeyReleased) {
+		if (event.key.code == sf::Keyboard::Escape) {
 			window.close();
-			break;
-		case Event::KeyReleased: {
-			if (event.key.code == sf::Keyboard::Escape) {
-				window.close();
-			}
-			break;
-		}
-		default:
-			break;
 		}
 	}
+	if (event.type == Event::MouseButtonReleased) {
+		if (event.mouseButton.button == sf::Mouse::Left) {
+			
+		}
+	}
+	if (event.type == Event::MouseMoved) {
+		float boxSizeX = searchBox.getSize().x/2;
+		float boxSizeY = searchBox.getSize().y/2;
+		searchBox.setPosition(event.mouseMove.x - boxSizeX, event.mouseMove.y-boxSizeY);
+		searchArea = Boundary(Point(event.mouseMove.x - boxSizeX, event.mouseMove.y - boxSizeY),Point(event.mouseMove.x + boxSizeX, event.mouseMove.y + boxSizeY));
+	}
+}
 
+sf::CircleShape getPoint(const Point& point) {
+	sf::CircleShape circle(4);
+	circle.setOrigin(-sf::Vector2f(point.x - 4, point.y - 4));
+	circle.setFillColor(sf::Color::Green);
+	return circle;
 }
